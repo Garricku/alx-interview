7b1c73ea309ee8a12f01abb1cb3d660b56e7d7c6
Garrick Larson Potts <ricki.glp@gmail.com>
2024-06-19 15:18:28 +0200

    task 0 iterview question

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    """Make total with the coins in the fewset coins possible"""
+    coins.sort(reverse=True)
+    num_coins = 0
+
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
+
+    if total == 0:
+        return num_coins
+    else:
+        return -1  
